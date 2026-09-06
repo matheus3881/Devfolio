fff76da0003b113d1f6ffd5c4991928c7f8c316c
Sant <matheus3881.ms@gmail.com>
2026-09-05 23:24:33 -0300

    fix: Corrige para poder rodar na vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+  // output: 'export',
+  // distDir: 'out',
   images: {
-    unoptimized: true,
+    // unoptimized: true,
   },
-  // Headers de segurança serão aplicados via _headers no deploy estático
-  // (não use headers() com output: 'export')
-};
+}
 
 export default nextConfig;
